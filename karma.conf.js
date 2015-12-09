dfcc38533fec2d56165859fefc61200e8882eebe
Andrew Dacenko <a.s.dvornik@gmail.com>
2015-12-09 04:43:18 +0200

    add(coverage)

--- karma.conf.js
+++ karma.conf.js
@@ -29,13 +29,21 @@ module.exports = function(config) {
 
     // preprocess matching files before serving them to the browser
     // available preprocessors: https://npmjs.org/browse/keyword/karma-preprocessor
-    preprocessors: {},
+    preprocessors: {
+        'src/js/**/!(*.spec).js': ['coverage']
+    },
 
 
     // test results reporter to use
     // possible values: 'dots', 'progress'
     // available reporters: https://npmjs.org/browse/keyword/karma-reporter
-    reporters: ['dots'],
+    reporters: ['dots', 'coverage'],
+
+
+    coverageReporter: {
+      type : 'lcov',
+      dir : 'coverage/'
+    },
 
 
     // web server port
